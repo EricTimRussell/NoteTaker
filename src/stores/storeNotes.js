3b8fe897ff7f078de2eea6238d0136ed67d767f0
Eric Russell <Erictim.Russell@live.com>
2024-01-23 13:30:34 -0700

    pinia setup

---
+++ src/stores/storeNotes.js
@@ -0,0 +1,30 @@
+import { defineStore } from 'pinia'
+
+export const useStoreNotes = defineStore('storeNotes' , {
+  state: () => {
+    return {
+      notes: [
+        {
+          id: 1,
+          title: "Title 1",
+          content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id suscipit voluptate, ut nihil labore facilis vero praesentium repudiandae officia quo, distinctio ducimus! Error fugiat quaerat pariatur, non voluptatibus accusantium unde."
+        },
+        {
+          id: 2,
+          title: "Title 2",
+          content: "Id suscipit voluptate, ut nihil labore facilis vero praesentium repudiandae officia quo, distinctio ducimus! Error fugiat quaerat pariatur, non voluptatibus accusantium unde."
+        },
+        {
+          id: 3,
+          title: "Title 3",
+          content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id suscipit voluptate, ut nihil labore facilis vero praesentium."
+        },
+        {
+          id: 4,
+          title: "Title 4",
+          content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id suscipit voluptate, ut nihil labore facilis vero praesentium repudiandae officia quo, distinctio ducimus! Error fugiat quaerat pariatur, non voluptatibus accusantium unde. this is an even longer note like if i was really writing about something important in a lot of detail and what not."
+        }
+      ]
+    }
+  }
+})
