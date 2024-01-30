03a648736ba37ae60c4a85774a03b8fa6dbb157e
Eric Russell <Erictim.Russell@live.com>
2024-01-30 08:30:49 -0700

    get notes from database

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,14 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import NavbarCompnent from "./components/NavbarCompnent.vue";
+import { useStoreNotes } from "./stores/storeNotes";
+import { onMounted } from "vue";
+
+const storeNotes = useStoreNotes()
+
+onMounted(() => {
+  storeNotes.getNotes()
+})
 </script>
 
 <style>
--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -1,33 +1,27 @@
 import { defineStore } from 'pinia'
 import { db } from '@/firebase'
+import { collection, getDocs } from "firebase/firestore";
+
 export const useStoreNotes = defineStore('storeNotes' , {
   state: () => {
     return {
       notes: [
-        {
-          id: "1",
-          title: "Title 1",
-          content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id suscipit voluptate, ut nihil labore facilis vero praesentium repudiandae officia quo, distinctio ducimus! Error fugiat quaerat pariatur, non voluptatibus accusantium unde."
-        },
-        {
-          id: "2",
-          title: "Title 2",
-          content: "Id suscipit voluptate, ut nihil labore facilis vero praesentium repudiandae officia quo, distinctio ducimus! Error fugiat quaerat pariatur, non voluptatibus accusantium unde."
-        },
-        {
-          id: "3",
-          title: "Title 3",
-          content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id suscipit voluptate, ut nihil labore facilis vero praesentium."
-        },
-        {
-          id: "4",
-          title: "Title 4",
-          content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id suscipit voluptate, ut nihil labore facilis vero praesentium repudiandae officia quo, distinctio ducimus! Error fugiat quaerat pariatur, non voluptatibus accusantium unde. this is an even longer note like if i was really writing about something important in a lot of detail and what not."
-        }
+      
       ]
     }
   },
   actions: {
+    async getNotes() {
+      const querySnapshot = await getDocs(collection(db, "notes"));
+        querySnapshot.forEach((doc) => {
+        let note = {
+          id: doc.id,
+          title: doc.data().title,
+          content: doc.data().content
+        }
+        this.notes.push(note)
+      })
+    },
     addNote(newNote){
       let currentDate = new Date().getTime()
       let note = {
